5e63bf233ab43ba0d8627de6f5e2b468603217f7
julielaursen <julielaursen1@gmail.com>
2023-05-07 11:38:21 -0500

    get first ocmponent working on react 18

---
+++ index.js
@@ -0,0 +1,11 @@
+function Hello() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <h1>Hello World!</h1>
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Hello />)
