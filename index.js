5ee791b090f97768bdfae9d84fc7222a20c2582e
julielaursen <julielaursen1@gmail.com>
2023-05-07 11:52:59 -0500

    video 13

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function Hello() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-      <h1>Hello World!</h1>
-      <h1>Hello World!</h1>
-    </div>
-  )
+function getMood() {
+  const moods = ['Angry', 'Hungry', 'Sleepy', 'Happy', 'Quiet']
+  return moods[Math.floor(Math.random() * moods.length)]
+}
+
+class JSXDemo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>My current mood is: {getMood()} </h1>
+      </div>
+    )
+  }
 }
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Hello />)
+root.render(<JSXDemo />)
