1ccda61567ea0b0cacbaeced19807627f9180cbe
julielaursen <julielaursen1@gmail.com>
2023-05-14 19:08:58 -0500

    finish pokegame

--- pokemon/pokemon/src/Pokecard.js
+++ pokemon/pokemon/src/Pokecard.js
@@ -12,9 +12,12 @@ class Pokecard extends Component {
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
-        <img src={imgSrc} alt={this.props.name} />
-        <div className="Pokecard-data">Type: {this.props.type}</div>
-        <div className="Pokecard-data">Exp: {this.props.exp}</div>
+
+        <div className="Pokecard-image">
+          <img src={imgSrc} alt={this.props.name} />
+        </div>
+        <div className="Pokecard-type">Type: {this.props.type}</div>
+        <div className="Pokecard-exp">Exp: {this.props.exp}</div>
       </div>
     )
   }
--- pokemon/pokemon/src/Pokegame.js
+++ pokemon/pokemon/src/Pokegame.js
@@ -11,23 +11,27 @@ class Pokegame extends Component {
       { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
       { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
       { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
-      { id: 133, name: 'Evee', type: 'normal', base_experience: 65 },
+      { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 },
     ],
   }
   render() {
     let hand1 = []
     let hand2 = [...this.props.pokemon]
+
     while (hand1.length < hand2.length) {
-      let randIndex = Math.floor(Math.random() * hand2.length)
-      let randPokemon = hand2.splice(randIndex, 1)[0]
+      let randIdx = Math.floor(Math.random() * hand2.length)
+      let randPokemon = hand2.splice(randIdx, 1)[0]
       hand1.push(randPokemon)
     }
-    console.log(hand1)
-    console.log(hand2)
+
+    let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience, 0)
+
+    let exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience, 0)
+
     return (
       <div>
-        <Pokedex pokemon={hand1} />
-        <Pokedex pokemon={hand2} />
+        <Pokedex pokemon={hand1} exp={exp1} isWinner={exp1 > exp2} />
+        <Pokedex pokemon={hand2} exp={exp2} isWinner={exp2 > exp1} />
       </div>
     )
   }
